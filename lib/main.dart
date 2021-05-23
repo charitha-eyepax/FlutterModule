db46b36fc78b9292d61d39ea2942a3242027c4d8
Charitha Ratnayake <charitha.r@eyepax.com>
2021-05-24 00:30:14 +0530

    Added channel into main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+  static const platform = const MethodChannel('com.jachdev.flutter_module.host/login');
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,6 +31,21 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  static const platform = const MethodChannel('com.jachdev.flutter_module.host/login');
+
+  Future<void> _login() async {
+    try {
+      final int result = await platform.invokeMethod('login');
+      if(result == 0){
+        _showMyDialog();
+      }else{
+
+      }
+    } on PlatformException catch (e) {
+
+    }
+  }
+
   void _incrementCounter() {
     setState(() {
       _counter++;
@@ -36,7 +54,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+
     final node = FocusScope.of(context);
+
     final ButtonStyle flatButtonStyle = TextButton.styleFrom(
       primary: Colors.black87,
       backgroundColor: Colors.grey,
@@ -80,7 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
               padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
               child: TextButton(
                 style: flatButtonStyle,
-                onPressed: () { },
+                onPressed: () { _login(); },
                 child: Text('Login'),
               ),
             )
@@ -92,6 +112,34 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Login Success'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text('This is alert from flutter mobile once receive the ack from native app.'),
+                Text('Would you like to go to next page?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Approve'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
